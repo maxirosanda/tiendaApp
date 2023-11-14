8f0fe06d8ea9aec6ab0d9b44adb5fd0c86780206
maxirosanda <maxi_rosanda@hotmail.com>
2023-11-14 19:35:29 -0300

    armando login

--- navigation/BreadNavigator.jsx
+++ navigation/BreadNavigator.jsx
@@ -1,32 +1,54 @@
+import { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import CategoriesScreen from '../screens/CategoriesScreen'
 import BreadDetailScreen from '../screens/BreadDetailScreen'
 import CategoryBreadScreen from '../screens/CategoryBreadScreen'
 import CartScreen from "../screens/CartScreen"
+import AuthScreen from '../screens/user/AuthScreen'
 
-const Stack = createNativeStackNavigator()
+
+  const ShopStack = createNativeStackNavigator()
+
+  const ShopNavigator = () => {
+    return   <ShopStack.Navigator
+                  initialRouteName='Home'
+                  screenOptions={{
+                    headerStyle: {
+                      backgroundColor:"red"
+                    },
+                    headerTintColor:"blue",
+                    headerTitleStyle:{
+                      fontWeight:"bold"
+                    }
+                  }}
+              >
+              <ShopStack.Screen name="Home" component={CategoriesScreen} options={{ title: 'Nuestra Panaderia' }} />
+              <ShopStack.Screen name="BreadCategory" component={CategoryBreadScreen} options={({route}) => ({ title: route.params.name })} />
+              <ShopStack.Screen name="DetailBread" component={BreadDetailScreen} options={({route}) => ({ title: route.params.name })} />
+              <ShopStack.Screen name="Cart" component={CartScreen} />
+            </ShopStack.Navigator>
+  }
+
+  const AuthStack = createNativeStackNavigator()
+  const AuthNavigator = () => {
+      return <AuthStack.Navigator
+                initialRouteName="Login"
+                screenOptions={{
+                  headerSown:false
+                }}
+             >
+                <AuthStack.Screen name="login" component={AuthScreen}/>
+             </AuthStack.Navigator>
+  }
 
  const BreadNavigator = () => {
+    const [user,setUser] = useState(null)
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Home'
-        screenOptions={{
-          headerStyle: {
-            backgroundColor:"red"
-          },
-          headerTintColor:"blue",
-          headerTitleStyle:{
-            fontWeight:"bold"
-          }
-        }}
-      >
-        <Stack.Screen name="Home" component={CategoriesScreen} options={{ title: 'Nuestra Panaderia' }} />
-        <Stack.Screen name="BreadCategory" component={CategoryBreadScreen} options={({route}) => ({ title: route.params.name })} />
-        <Stack.Screen name="DetailBread" component={BreadDetailScreen} options={({route}) => ({ title: route.params.name })} />
-        <Stack.Screen name="Cart" component={CartScreen} />
-      </Stack.Navigator>
+      {
+        user ? <ShopNavigator/> : <AuthNavigator/>
+      }
     </NavigationContainer>
   )
 }
